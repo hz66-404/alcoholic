ede7ca908ccff3365a298fa371163e56b32be3b2
Han <zhenghannah404@gmail.com>
2025-06-22 22:22:07 +0100

    import tempfile

--- pages/upload.py
+++ pages/upload.py
@@ -8,6 +8,8 @@ from supabase import create_client, Client
 import uuid
 import io
 import os
+import tempfile
+
 # ✅ Supabase 设置
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
